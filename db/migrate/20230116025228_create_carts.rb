d58f2ed0bfac1340ff335756404559f9cb4f8bdd
May Chen <maymchen1@gmail.com>
2023-01-15 21:58:23 -0500

    m

---
+++ db/migrate/20230116025228_create_carts.rb
@@ -0,0 +1,10 @@
+class CreateCarts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :carts
+    do |t|
+      t.string :status,
+      default :"current"
+      t.timestamps
+    end
+  end
+end
