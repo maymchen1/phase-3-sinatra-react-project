d4be70630cb6e81c65301a1fd965797a756acf18
May Chen <maymchen1@gmail.com>
2023-01-19 19:31:22 -0500

    m

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,25 @@
-class CartsController < ApplicationController
+class CartsController < ApplicationController   
+    get "/carts/:id"
+    find_cart
+    @cart.to_json(include:[:items])
+    end
 
+    post "carts" do
+        if Cart.last.status != "current"
+        @cart = Cart.create
+        @cart.to_json(include:[:items])
+        else
+            Cart.last.to_json(include:[:items])
+        end
+    end
+
+    patch "carts/:id" do
+        find_cart 
+        @cart.update(status: "complete")
+        @cart.to_json(include:[:items])
+    end
+
+    private
+    def find_cart
+        @cart = Cart.find_by_id(params[:id])
 end
