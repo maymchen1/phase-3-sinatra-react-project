057a1fa73c15ec6b3b77a4503e8af7359d2d2478
May Chen <maymchen1@gmail.com>
2023-01-19 19:59:34 -0500

    m

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,32 @@
 class ItemsController < ApplicationController
+    get "/items" do
+        @items = Item.not_in_cart
+        @items.to_json
+    end
+    post "/items" do
+        @item = Item.new(params[:item])
+        if @item.save
+            @item.to_json
+        else
+            {error: @item.errors.full_messages}.to_json
+        end
+    end
+    patch "/items/:id" do 
+        find_item
+        if @item.update(params)
+            @item.to_json
+        end
 end
+delete "/items/:id" do
+    find_item
+    if @item
+        @item.destroy
+        @item.to_json
+    end
+    end
+private
+def find_item
+    @item = Item.find_by_id(params[:id])
+end
+end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base 
     belongs_to :cart
+    validates :title, presence: true
+    def self.not_in_cart
+    where(cart_id: nil)
+    end
 end
