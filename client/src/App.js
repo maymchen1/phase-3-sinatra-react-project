2fc36f4e92477ab523958d2da7566dd6fc998d94
May Chen <maymchen1@gmail.com>
2023-01-19 20:18:30 -0500

    m

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ViewCart from "./components/carts/ViewCart";
+import ItemList from "./components/items/ItemList";
+import Navbar from "./components/navigation/Navbar";
 import Home from "./components/static/Home";
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/items" element={<ItemList />} />
+        <Route path="/checkout" element={<ViewCart />} />
       </Routes>
       </Router>
   );
---
+++ client/src/components/navigation/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/items">Items</Link></li>
+        <li><Link to="/checkout">View Cart</Link></li>
+    </ul>
+  )
+}
+
+export default Navbar
